dfb58acf136e2001e8b075edc1ae3172b0a2bc44
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-12-11 18:56:17 -0500

    wrote out group controller actions

--- app/controllers/v1/groups_controller.rb
+++ app/controllers/v1/groups_controller.rb
@@ -2,24 +2,46 @@ class V1::SessionsController < ApplicationController
     before_action :authenticaticate_user!
 
     def create
-        user = Users.all.find_by[id: params[:id]]
-
+        user = Users.find_by[id: params[:id]]
+        @group = user.build_group(group_params)
+        if @group.save
+            render :create
+        else
+            render json: {errors: @group.errors.full_messages}
+        end
     end
 
     def index
-
+        @groups = Group.all
+        render :index
     end
 
     def show
-
+        @group = Group.find_by(id: params[:id])
+        if @group
+            render :show
+        else
+            render json: {errors: @group.errors.full_messages}
+        end
     end
 
     def update
-
+        @group = Group.find_by(id: params[:id])
+        if @group.update(group_params)
+            render :update
+        else
+            render json: {errors: @group.errors.full_messages}
+        end
     end
 
     def destroy
-
+        group = Group.find_by(id: params[:id])
+        if group
+            group.destroy
+            render json: :ok
+        else
+            head(:unauthorized)
+        end
     end
 
     private
